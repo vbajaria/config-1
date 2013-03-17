c143ec8b0a3a47defffde8374b1ff0d23551eee6
Premal Shah <premal.j.shah@gmail.com>
2013-03-16 19:22:57 -0700

    beacon configs

---
+++ beacon/init.d
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+USAGE ()
+{
+  echo "Usage: $NAME {beacon-dev|beacon-prod|beacon-prod-https} {start|stop}" >&2
+}
+
+if [ -z $2 ]; then
+  USAGE
+  exit 1
+fi
+
+my_dir=`dirname $0`
+. $my_dir/service_config.sh
+
+SERVICE=$1
+
+case "$2" in
+  start)
+    START "java -cp /usr/lib/beacon/dataapi-0.1.jar io.ntropy.dataapi.DataApiService server /usr/lib/beacon/$1-config.yml"
+  ;;
+  stop)
+    STOP  "java -cp /usr/lib/beacon/dataapi-0.1.jar io.ntropy.dataapi.DataApiService server /usr/lib/beacon/$1-config.yml"
+  ;;
+  *)
+    USAGE
+    exit 1
+  ;;
+esac
+
+exit 0
+

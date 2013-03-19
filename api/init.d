3780c1e207433b8ad74cbd26483af053c9a7069d
Premal Shah <premal.j.shah@gmail.com>
2013-03-19 11:31:34 -0700

    api config

---
+++ api/init.d
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+USAGE ()
+{
+  echo "Usage: $NAME {dev|prod-ssl} {start|stop}" >&2
+}
+
+if [ -z $2 ]; then
+  USAGE
+  exit 1
+fi
+
+my_dir=`dirname $0`
+. $my_dir/service_config.sh
+
+SERVICE=$1
+
+case "$2" in
+  start)
+    START "java -cp /usr/lib/api/dataapi-0.1.jar io.ntropy.dataapi.DataApiService server /usr/lib/api/$1-api-config.yml"
+  ;;
+  stop)
+    STOP "java -cp /usr/lib/api/dataapi-0.1.jar io.ntropy.dataapi.DataApiService server /usr/lib/api/$1-api-config.yml"
+  ;;
+  *)
+    USAGE
+    exit 1
+  ;;
+esac
+
+exit 0
+
+~

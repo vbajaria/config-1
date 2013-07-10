383d47fe970fc83b4f5a00f65717ab50faccd39e
Viral Bajaria <viral@ntropy.io>
2013-07-09 22:52:24 -0700

    add correct logging to all recipe scripts and some cleanup

--- check_and_run.sh
+++ check_and_run.sh
@@ -2,31 +2,36 @@
 
 NAME="./check_and_run.sh"
 
-STORM_CLASSPATH=/usr/lib/storm/storm-recipes-0.1-jar-with-dependencies.jar:/usr/lib/storm/storm-0.9.0-wip16.jar:/usr/lib/storm/lib/*:/usr/lib/storm/conf/
+STORM_CLASSPATH=/usr/lib/storm/storm-recipes-0.1-jar-with-dependencies.jar:/usr/lib/storm/storm-0.9.0-wip16.jar:/usr/lib/storm/lib/*:/usr/lib/storm/conf/:/usr/lib/storm/logback/
+JAVA_OPTS="-Dlogback.configurationFile=/usr/lib/storm-0.9.0-wip16/logback/cluster.xml -Dlogfile.name=$1.log"
 
 RECIPE_RUNNER()
 {
-  java -Dstorm.conf.file=storm.yaml -cp $STORM_CLASSPATH io.ntropy.management.RecipeRunner >> /mnt/storm/logs/recipe-runner
+  RUN io.ntropy.management.RecipeRunner /mnt/storm/logs/recipe-runner
 }
 
 RECIPE_MONITOR()
 {
-  java -Dstorm.conf.file=storm.yaml -cp $STORM_CLASSPATH io.ntropy.management.RecipeMonitor >> /mnt/storm/logs/recipe-monitor
+  RUN io.ntropy.management.RecipeMonitor /mnt/storm/logs/recipe-monitor
 }
 
 RECIPE_KILLER()
 {
-  java -Dstorm.conf.file=storm.yaml -cp $STORM_CLASSPATH io.ntropy.management.RecipeKiller >> /mnt/storm/logs/recipe-killer
+  RUN io.ntropy.management.RecipeKiller /mnt/storm/logs/recipe-killer
 }
 
 UPDATE_DIMENSION_CARDINALITY()
 {
-  java -Dstorm.conf.file=storm.yaml -cp $STORM_CLASSPATH io.ntropy.management.UpdateDimensionCardinality >> /mnt/storm/logs/recipe-update-cardinality
+  RUN io.ntropy.management.UpdateDimensionCardinality /mnt/storm/logs/recipe-update-cardinality
 }
 
 POPULATE_BASELINE_DIMENSIONS()
 {
-  java -Dstorm.conf.file=storm.yaml -cp $STORM_CLASSPATH io.ntropy.management.PopulateBaselineDimensions >> /mnt/storm/logs/recipe-populate-baseline
+  RUN io.ntropy.management.PopulateBaselineDimensions /mnt/storm/logs/recipe-populate-baseline
+}
+
+RUN() {
+  java $JAVA_OPTS -Dstorm.conf.file=storm.yaml -cp $STORM_CLASSPATH $1 >> $2
 }
 
 USAGE()

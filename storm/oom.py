b09c8f9c9ef677c09cf1161cbd98d73eece0d0a8
Premal Shah <premal.j.shah@gmail.com>
2013-07-16 12:38:59 -0700

    updated error and oom scripts

--- storm/oom.py
+++ storm/oom.py
@@ -2,7 +2,7 @@
 
 import datetime, sys
 
-f = open('oom.log', 'w')
+f = open('/mnt/storm/oom.log', 'w')
 f.write('Worker %s ran out of memory at %s\n' %(sys.argv[1], str(datetime.datetime.now())))
 f.close()
 

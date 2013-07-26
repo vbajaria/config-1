94b21a130b5a1f4aeafd48954bef500750923fd7
Premal Shah <premal.j.shah@gmail.com>
2013-07-25 22:48:49 -0700

    added kafka cron and backup script

---
+++ kafka/backup-kafka.py
@@ -0,0 +1,34 @@
+import os, boto, datetime, socket
+from subprocess import Popen, PIPE
+from boto.s3.key import Key
+
+AWS_ACCESS_KEY_ID = 'AKIAJZX3DTK5VLHPLE3Q'
+AWS_SECRET_ACCESS_KEY = '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'
+
+dt = datetime.date.today() - datetime.timedelta(days=1)
+conn = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+bucket = conn.get_bucket('grepdata-kafka')
+
+for dir, s, files in os.walk('/mnt/data/kafka'):
+    for f in files:
+        local_filename = '%s/%s' %(dir, f)
+        s3_filename = 'REPLACE_WITH_KAFKA_SERVER%s' %local_filename
+
+        local_size  = os.stat(local_filename).st_size
+        remote_size = 0
+
+        try:
+            remote_size = bucket.lookup(s3_filename).size             
+
+            if local_size == remote_size:
+                print '\tSkipping %s' %local_filename
+                continue
+        except AttributeError:
+            pass
+    
+        print 'Uploading %s (%d bytes)' %(local_filename, (local_size - remote_size))
+
+        key = Key(bucket)
+        key.key = s3_filename
+        key.set_contents_from_filename(local_filename)
+
